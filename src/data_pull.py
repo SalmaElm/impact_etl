32febdc3236314fedeec8b9af093dfe7c10b41e5
SalmaElm <salma@seed.com>
2023-12-25 09:47:05 -0700

    fixed email notification

--- src/data_pull.py
+++ src/data_pull.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import csv
-from datetime import datetime
+from datetime import datetime as dt
 import base64
 import boto3
 from botocore.exceptions import NoCredentialsError
@@ -12,24 +12,40 @@ from boto3.s3.transfer import S3Transfer
 import logging
 import os
 from dotenv import load_dotenv
-from utilities import send_email  # Import the send_email function from utilities.py
+from utilities import *  # Import the send_email function from utilities.py
 
 # Set up the email content
 subject = "Impact Log File"
 body = "Please find attached the log file."
 
-# Configure logging to a file in a 'logs' directory
-log_directory = os.path.abspath(os.path.join(__file__, "../..", "logs"))
-os.makedirs(log_directory, exist_ok=True)
-log_file_path = os.path.join(log_directory, "impact_log.log")
-# Set up logging
-log_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "logs"))
-file_handler = logging.FileHandler(log_file_path)
+# # Configure logging to a file in a 'logs' directory
+# log_directory = os.path.abspath(os.path.join(__file__, "../..", "logs"))
+# os.makedirs(log_directory, exist_ok=True)
+# log_file_path = os.path.join(log_directory, "impact_log.log")
+# # Set up logging
+# log_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "logs"))
+# file_handler = logging.FileHandler(log_file_path)
 
-# Add a formatter and a file handler to the logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler(log_file_path, mode='a')])
-logger = logging.getLogger(__name__)
-logger.addHandler(file_handler)
+# # Add a formatter and a file handler to the logger
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler(log_file_path, mode='a')])
+# logger = logging.getLogger(__name__)
+# logger.addHandler(file_handler)
+
+#########################################################################################################
+# Error Logging
+#########################################################################################################
+path = os.path.abspath(os.path.join(__file__ ,"../.."))+'/logs'
+
+with open(os.path.join(path,'impact_log.log'), 'w') as logfile:
+    pass
+logging.basicConfig(filename=f'{path}/impact_log.log', level=logging.INFO, 
+                format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+logger=logging.getLogger(__name__)     
+
+file_ts = datetime.datetime.now().strftime("%y%m%d%H%M%S")
+
+logger.info(f'Impact report data load begins at {str(datetime.datetime.now())}...')
 
 file_path = os.path.abspath(os.path.join(__file__ ,"../.."))
 env_path = f'{file_path}/src/.env'
@@ -40,8 +56,8 @@ sender_email = "salma@seed.com"  # Your Gmail Enterprise email address
 app_password = os.environ.get('app_password')  # If you have two-factor authentication enabled
 recipient_email = "salma@seed.com"  # Email address to send the log file to
 
-end_date = datetime.now().strftime('%Y-%m-%d')
-file_ts = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+end_date = dt.now().strftime('%Y-%m-%d')
+file_ts = dt.now().strftime('%Y-%m-%d_%H:%M:%S')
 # Your S3 bucket name
 s3_bucket_name = 'impact-performance-data'
 s3_file_name = f'performance_{file_ts}.csv'
@@ -111,7 +127,7 @@ def fetch_and_filter_data():
     account_sid = os.environ.get('account_sid')
     auth_token = os.environ.get('auth_token')
 
-    end_date = datetime.now().strftime('%Y-%m-%d')
+    end_date = dt.now().strftime('%Y-%m-%d')
 
     base_url = f'https://IRgqMP5TEkmE4304993FGxhxf6x2xHBsb1:uFNo8XDUvvQYd6RSDmGzCievx%7ENDYB%7EB@api.impact.com/Advertisers/IRgqMP5TEkmE4304993FGxhxf6x2xHBsb1/ReportExport/att_adv_performance_by_day_pm_only.json?START_DATE=2023-07-01&END_DATE={end_date}&SUBAID=19848'
     headers = {
@@ -164,16 +180,10 @@ def fetch_and_filter_data():
 
         except (json.JSONDecodeError, csv.Error) as e:
             logger.error(f'Error: {e}')
-            email_subject = "ETL Job Status - Error"
-            email_body = f'Error: {e}'
-            send_email(email_subject, email_body, sender_email, app_password, recipient_email)
             return f"Error: {e}"
 
     else:
         logger.error(f"Error: {response.status_code}\nResponse content: {response.text}")
-        email_subject = "ETL Job Status - Error"
-        email_body = f"Error: {response.status_code}\nResponse content: {response.text}"
-        send_email(email_subject, email_body, sender_email, app_password, recipient_email)
         return f"Error: {response.status_code}\nResponse content: {response.text}"
         
 
@@ -207,15 +217,9 @@ def upload_to_s3(data, s3_file_name):
 
 
         logger.info(f"File uploaded successfully to {s3_bucket_name}/{s3_file_name}")
-        email_subject = "ETL Job Status"
-        email_body = f"File uploaded successfully to {s3_bucket_name}/{s3_file_name}"
-        send_email(email_subject, email_body, sender_email, app_password, recipient_email)
 
     except Exception as e:
         logger.error(f"Error uploading file to S3: {e}")
-        email_subject = "ETL Job Status - Error"
-        email_body = f"Error uploading file to S3: {e}"
-        send_email(email_subject, email_body, sender_email, app_password, recipient_email)
 
 
 # Fetch and filter data
@@ -225,3 +229,10 @@ filtered_data_result = fetch_and_filter_data()
 
 upload_to_s3(filtered_data_result, s3_file_name)
 update_snowflake_table(filtered_data_result)
+
+count = email_wrapper(file_path)
+                                      
+if count <= 0:                
+    send_email(file_path, sender_email, recipient_email, subject, app_password)  
+else:
+    send_failure_email(file_path, sender_email, recipient_email, subject, app_password) 
--- src/utilities.py
+++ src/utilities.py
@@ -1,8 +1,25 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
+import os
+import logging
+import datetime
+from datetime import date
+
+# Error Logging
+path = os.path.abspath(os.path.join(__file__, "../..")) + '/logs'
+
+with open(os.path.join(path, 'impact_log.log'), 'w'):
+    pass
+
+logging.basicConfig(filename=f'{path}/impact_log.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+logger = logging.getLogger(__name__)
+
+file_ts = datetime.datetime.now().strftime("%y%m%d%H%M%S")
+
+logger.info(f'Impact report data load begins at {str(datetime.datetime.now())}...')
 
 
 def send_email(subject, body, sender_email, app_password, recipient_email, attachment_path=None):
@@ -12,11 +29,157 @@ def send_email(subject, body, sender_email, app_password, recipient_email, attac
     message["To"] = recipient_email
     message["Subject"] = subject
 
-        # Attach the log content as plain text in the email body
+    # Attach the log content as plain text in the email body
     message.attach(MIMEText(body, "plain"))
-    
+
     # Connect to the SMTP server and send the email
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
         server.login(sender_email, app_password)  # Use your App Password here if needed
-        server.sendmail(sender_email, recipient_email, message.as_string())
+        server.sendmail(sender_email, recipient_email, message)
+
+
+# Email wrapper for error handling
+def email_wrapper(file_path):
+    directory = file_path + '/logs'
+
+    search_phrases = ["ERROR"]
+    count = 0
+
+    for filename in os.listdir(directory):
+
+        if filename.endswith('.log'):
+
+            path = os.path.join(directory, filename)
+
+            with open(path, "r") as fp:
+                for line in fp:
+                    for phrase in search_phrases:
+                        if phrase in line:
+                            count = +1
+
+    return count
+
+
+# Send confirmation email on SUCCESS
+def send_email(file_path, sender_email, recipient_email, subject, app_password):
+    COMMASPACE = ', '
+    directory = f'{file_path}/logs'
+
+    sender = f'{sender_email}'
+    recipients = recipient_email.split(',')
+
+    # Create the enclosing (outer) message
+    outer = MIMEMultipart()
+
+    outer['Subject'] = f'{subject} Automation - SUCCESS -  {date.today()}'
+
+    outer['To'] = COMMASPACE.join(recipients)
+    outer['From'] = sender_email
+
+    logger.info('Sending E-mail notification...')
+    logger.info(f'{subject} data processing is completed at {str(datetime.datetime.now())}')
+
+    email_body = ""
+
+    for filename in os.listdir(directory):
+
+        if filename.endswith('.log'):
+
+            path = os.path.join(directory, filename)
+
+            with open(path, "r") as fp:
+                data = fp.read()
+
+            content = data.splitlines()
+
+            for element in content:
+                log_parts = element.split(" ", 3)
+                email_body += f'<tr><td>{log_parts[0]}</td><td>{log_parts[1]}</td><td>{log_parts[2]}</td><td>{log_parts[3]}</td></tr>'
+
+    html = f"""<html>
+                <body>
+                    <table border="1">
+                        <tr>
+                            <th>Date</th>
+                            <th>Timestamp</th>
+                            <th>Log Level</th>
+                            <th>Log Message</th>
+                        </tr>
+                        {email_body}
+                    </table>
+                </body>
+              </html>"""
+
+    msg = MIMEText(html, 'html')
+    outer.attach(msg)
+
+    message = outer.as_string()
+
+    # Connect to the SMTP server and send the email
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(sender_email, app_password)  # Use your App Password here if needed
+        server.sendmail(sender_email, recipient_email, message)
+
+
+# Send confirmation email on FAILURE
+def send_failure_email(file_path, sender_email, recipient_email, subject, app_password):
+    COMMASPACE = ', '
+    directory = f'{file_path}/logs'
+
+    sender = f'{sender_email}'
+    recipients = recipient_email.split(',')
+
+    # Create the enclosing (outer) message
+    outer = MIMEMultipart()
+
+    outer['Subject'] = f'{subject} Automation - FAILURE -  {date.today()}'
+
+    outer['To'] = COMMASPACE.join(recipients)
+    outer['From'] = sender_email
+
+    logger.info('Sending E-mail notification...')
+    logger.info(f'{subject} data processing is completed with error(s) at {str(datetime.datetime.now())}')
+
+    email_body = ""
+
+    for filename in os.listdir(directory):
+
+        if filename.endswith('.log'):
+
+            path = os.path.join(directory, filename)
+
+            with open(path, "r") as fp:
+                data = fp.read()
+
+            content = data.splitlines()
+
+            for element in content:
+                log_parts = element.split(" ", 3)
+                email_body += f'<tr><td>{log_parts[0]}</td><td>{log_parts[1]}</td><td>{log_parts[2]}</td><td>{log_parts[3]}</td></tr>'
+
+    html = f"""<html>
+                <body>
+                    <table border="1">
+                        <tr>
+                            <th>Date</th>
+                            <th>Timestamp</th>
+                            <th>Log Level</th>
+                            <th>Log Message</th>
+                        </tr>
+                        {email_body}
+                    </table>
+                </body>
+              </html>"""
+
+    msg = MIMEText(html, 'html')
+    outer.attach(msg)
+
+    message = outer.as_string()
+
+    # Connect to the SMTP server and send the email
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(sender_email, app_password)  # Use your App Password here if needed
+        server.sendmail(sender_email, recipient_email, message)
